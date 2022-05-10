13dd346f51b4b954e683fe3df8a400b1aae05eb9
nguyenquoctuan110398 <nguyenquoctuan11031998@gmail.com>
2022-05-10 11:44:45 +0700

    10/5/2022

--- case_study/component-case-study/src/app/app.module.ts
+++ case_study/component-case-study/src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {FormsModule} from "@angular/forms";
-import {NavbarComponent} from "./navbar/navbar.component";
-import {HeaderComponent} from "./header/header.component";
+import {NavbarComponent} from "./component/layout/navbar/navbar.component";
+import {HeaderComponent} from "./component/layout/header/header.component";
 import {ServicesComponent} from "./component/services/services.component";
 import {CreateServiceComponent} from "./component/services/create-service/create-service.component";
 import {ListCustomerComponent} from "./component/customer/list-customer/list-customer.component";
@@ -14,7 +14,7 @@ import {CreateCustomerComponent} from "./component/customer/create-customer/crea
 import {EditCustomerComponent} from "./component/customer/edit-customer/edit-customer.component";
 import {CreateContractComponent} from "./component/contract/create-contract/create-contract.component";
 import {ListContractComponent} from "./component/contract/list-contract/list-contract.component";
-import {FooterComponent} from "./footer/footer.component";
+import {FooterComponent} from "./component/layout/footer/footer.component";
 import { EditServiceVillaComponent } from './component/services/edit-service-villa/edit-service-villa.component';
 import { EditServiceHouseComponent } from './component/services/edit-service-house/edit-service-house.component';
 import { EditServiceRoomComponent } from './component/services/edit-service-room/edit-service-room.component';
